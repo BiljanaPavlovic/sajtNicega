e4680ecdf8cf6f4bd144783fcac3deb8ebdb9859
Biljana <zabiljanupavlovic@gmail.com>
2021-02-16 23:18:00 +0100

    search added

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -1,12 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Movie = ({ name, price }) => {
-  
-
   return (
     <div>
-      <h3 className = 'movie'>{name}</h3>
-      <p className = 'moviePrice' >{price}</p>
+      <h3 className="movie">{name}</h3>
+      <p className="moviePrice">{price}</p>
     </div>
   );
 };
--- src/components/movieList.jsx
+++ src/components/movieList.jsx
@@ -1,22 +1,46 @@
-import React,  { useState, useContext } from "react";
+/** @format */
+
+import React, { useContext, useEffect, useState } from "react";
 import Movie from "./movie";
 import { MovieContext } from "./movieContext";
 import AddMovie from "./addMovie";
 import Nav from "./nav";
 
-
 const MovieList = () => {
-    const [movies, movielist] = useContext(MovieContext)
+  const [movies] = useContext(MovieContext);
+  const [search, setSearch] = useState("");
+  const [filteredMovie, setFilteredMovie] = useState([]);
 
-return (
+  useEffect(() => {
+    setFilteredMovie(
+      movies.filter((movie) =>
+        movie.name.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search, movies]);
+
+  return (
     <div>
-<Nav />
-{movies.map(movie => (
-<Movie name={movie.name} price={movie.price} key= {movie.id}/>
-))}
-<AddMovie />
-</div>
-);
-}
+      <Nav />
+      <input
+        type="text"
+        placeholder="Search Movies"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {filteredMovie.map((movie, idx) => (
+        <Movie
+          name={movie.name}
+          price={movie.price}
+          key={movie.id}
+          {...movies}
+        />
+      ))}
+      {movies.map((movie) => (
+        <Movie name={movie.name} price={movie.price} key={movie.id} />
+      ))}
+      <AddMovie />
+    </div>
+  );
+};
 
-export default MovieList
+export default MovieList;
